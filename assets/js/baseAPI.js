fe843a7f453fe086005c75a6234d187229642a00
parvebu <675385289@qq.com>
2020-10-30 20:50:44 +0800

    完成了主页功能的开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -5,5 +5,31 @@
 $.ajaxPrefilter(function (options) {
     //在发起真正的ajax请求之前 统一萍姐请求的根路径
     options.url = 'http://ajax.frontend.itheima.net' + options.url
-    console.log(options.url);
+    // console.log(options.url);
+
+
+    //统一为有权限 的接口 设置headers 请求头
+
+    if (options.url.indexOf('/my/') !== -1) {
+        options.headers = {
+            Authorization: localStorage.getItem('token') || ''
+        }
+    }
+
+
+    //全局同意挂载 complete 回调函数
+    //不论成功还是失败 最终都会调用complete 回调函数
+    options.complete = function (res) {
+        // console.log('执行了complete回调')
+        // console.log(res);
+        // 在 complete回调函数中，可以使用res.responseJSON拿到服务器响应回来的数据
+
+
+        if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+            //强制清空token
+            localStorage.removeItem('token')
+            //2.强制跳转到登陆页面
+            location.href = '/login.html'
+        }
+    }
 })
